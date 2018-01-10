5f8452900ed7547b474575c3cfaf7d3bb93bd038
Wilson Chen <unruledboy@users.noreply.github.com>
2018-01-10 21:26:05 +1100

    Create WordMatch.cs

---
+++ WordMatch.cs
@@ -0,0 +1,10 @@
+namespace WuManberNet
+{
+	public struct WordMatch
+	{
+		public int Index { get; set; }
+		public string Word { get; set; }
+		public int Id { get; set; }
+		public string Tag { get; set; }
+	}
+}
